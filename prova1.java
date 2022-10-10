3d36539d9bc6a678bfc30991258698d172217c13
fraaaaa4 <sitipubblicitari01@gmail.com>
2022-10-10 08:46:58 +0000

    Everything's been committed greaty

--- prova1.java
+++ prova1.java
@@ -1,11 +1,24 @@
 public class prova1{
-    
+
     public static void main(String[]args) {
         // i have to leave this in any program
         // public or private state: as VB6
 
     System.out.println("something");
-    system.out.println("another something")
-    system.out.println("this is like vb6")
+
     /* system.out.print = prints on the screen a line
     system.out.println = goes down a row and prints a line*/
+
+    int n1;
+    double n2;
+    
+    // make the scanner library actually read your text input //
+
+        n1 = 25;
+        n2 = 52.04;
+
+        System.out.println("Here's a summary of what you've written");
+        System.out.println(n1+n2);
+    }
+
+}
