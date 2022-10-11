d5d0025579765d6e3620072dc3a43bc74120d84d
fraaaaa4 <sitipubblicitari01@gmail.com>
2022-10-11 17:03:09 +0000

    Tried and failed to do an error handler I want "On error Resume" back

--- Hobby projects/Calc.java
+++ Hobby projects/Calc.java
@@ -2,7 +2,10 @@ import java.util.Scanner; //imports scanner object --> scans input from your key
 import java.lang.Math; //imports Mathematic functions
 public class Calc{
 
+
     public static void main(String[]args) {
+
+
 System.out.println("[°°°°°°Simple Calculator in Java°°°°°°] (10/10/2022)");
 
 char operator;
@@ -17,30 +20,36 @@ char operator;
             System.out.print("Enter here which operation you want to make (+, -, *, /, q for sqrt, p for pow, a for average): ");
         operator = Input.next().charAt(0); //asks for an operator
        
-        System.out.print("Enter here the second number: ");
-            number2 = Input.nextDouble();
 
             switch (operator) {
                 //performs addition between numbers
                 case '+':
+                System.out.print("Enter here the second number: ");
+                number2 = Input.nextDouble();
                 result = number1 + number2;
                 System.out.println(number1 + " + " + number2 + " = " + result);
                 break;
 
                 //performs subtraction between numbers
                 case '-':
+                System.out.print("Enter here the second number: ");
+                number2 = Input.nextDouble();
                 result = number1 - number2;
                 System.out.println(number1 + " - " + number2 + " = " + result);
                 break;
 
                 //performs multiplication between numbers
                 case '*':
+                System.out.print("Enter here the second number: ");
+                number2 = Input.nextDouble();
                 result = number1 * number2;
                 System.out.println(number1 + " * " + number2 + " = " + result);
                 break;
 
                 //performs division between numbers
                 case '/':
+                System.out.print("Enter here the second number: ");
+                number2 = Input.nextDouble();
                 result = number1 / number2;
                 resto = number1 % number2;
                 System.out.println(number1 + " / " + number2 + " = " + result + " with a rest of " + resto);
@@ -49,19 +58,22 @@ char operator;
                 //performs sqrt between numbers...?
                 case 'q':
                Double resultsqrt1 = Math.sqrt(number1);
-                 Double resultsqrt2 = Math.sqrt(number2);
                 System.out.print("First sqrt value: " + resultsqrt1);
-                System.out.print("Second sqrt value:" + resultsqrt2);
+
                 break;
 
                 //performs pow
                 case 'p':
+                System.out.print("Enter here the second number: ");
+                number2 = Input.nextDouble();
                 double resultpow1 = Math.pow(number1, number2);
                 System.out.println(number1 + " power of " +number2 + " = " +resultpow1);
                 break;
 
                 //does an average between the two numbers
                 case 'a':
+                System.out.print("Enter here the second number: ");
+                number2 = Input.nextDouble();
                 double number3;
                 
                 System.out.print("Enter here the third number to do an average: ");
@@ -77,10 +89,18 @@ char operator;
 
             System.out.println(" ");
             System.out.println("Thank you for having used such simple, basic and poorly functional calculator.");
-            System.out.print("What you've been seeing is my testing projects for uni, which you can all check on my GitHub page at unimib.");
-            System.out.println("You'll see me back in Visual Basic 6 because it is faaaar better than Java for me");
+            System.out.println("What you've been seeing is my testing projects for uni, which you can all check on my GitHub page at unimib.");
+            System.out.println(" You'll see me back in Visual Basic 6 because it is faaaar better than Java for me");
 
             Input.close(); // after reading user input, you need to close it
-    }
 
-}
+
+            // try error handler
+            
+            }
+                }
+            
+            
+    
+
+
