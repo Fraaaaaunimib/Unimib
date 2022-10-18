c6111771fac0f5d434ca762906d0a45d503445a5
fraaaaa4 <sitipubblicitari01@gmail.com>
2022-10-18 08:41:04 +0000

    Big graphical updates to Calc

--- Hobby projects/Calc.java
+++ Hobby projects/Calc.java
@@ -90,8 +90,48 @@ static double eval(final String str) {
     public static void main(String[]args) {
 
 
+        final String ANSI_BOLD = "\u001B[1m"; // makes text afterwards bold
 
+        final String ANSI_BOLD_END = "\u001B[0m"; // makes the text return to its normal state
+        final String ANSI_ORANGE1 = "\u001B[38;2;255;127;80m";
+        final String ANSI_RESET2 = "\033[39m";
+
+        String start = """
+            ╔════════════════════════════════════════════════════════════════════════════════════════════════════╗                        
+            ║  This is a calculator.                                                                             ║
+            ╚════════════════════════════════════════════════════════════════════════════════════════════════════╝
+              @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@             
+              @@@                           @@@             
+              @@@                           @@@             
+              @@@                           @@@             
+              @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            This is a calculator 
+              @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@            A calculator calculates math stuff
+              @@@@@     @@@@     @@@@     @@@@@            Math stuff is not fun
+              @@@@@     *@@@     #@@      @@@@@            That's why there's a calculator for you
+              @@@@@@@@@@@@@%@&@@@&@@@@@@@@@&@@@            You should try the calculator
+              @@@@@@@@&@&@&@@&@&@@&@&%@@@@@@&@&            It's very fun to try the calculator 
+              @@@@@     (@@@     &@@      @@@@@            It'll change your life trying the calculator
+              @@@@@     @@@@     @@@@     @@@@@            This is the calculator. 
+              @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@             
+              @@@@@@   .@@@@&   *@@@@    @@@@@@             
+              @@@@@      @@@      @@      @@@@@             
+              @@@@@@   @@@@@@   @@@@@@  .@@@@@@             
+              @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@             
+                                                            
+                                                            
+                """;
     
+                // box drawing characters
+                /*
+     ─	━	│	┃	┄	┅	┆	┇	┈	┉	┊	┋	┌	┍	┎	┏
+    ┐	┑	┒	┓	└	┕	┖	┗	┘	┙	┚	┛	├	┝	┞	┟
+	┠	┡	┢	┣	┤	┥	┦	┧	┨	┩	┪	┫	┬	┭	┮	┯
+	┰	┱	┲	┳	┴	┵	┶	┷	┸	┹	┺	┻	┼	┽	┾	┿
+	╀	╁	╂	╃	╄	╅	╆	╇	╈	╉	╊	╋	╌	╍	╎	╏
+	═	║	╒	╓	╔	╕	╖	╗	╘	╙	╚	╛	╜	╝	╞	╟
+	╠	╡	╢	╣	╤	╥	╦	╧	╨	╩	╪	╫	╬	╭	╮	╯
+	╰	╱	╲	╳	╴	╵	╶	╷	╸	╹	╺	╻	╼	╽	╾	╿
+                 */
 
 
 char operator;
@@ -99,18 +139,18 @@ char operator;
 
         Scanner Input = new Scanner(System.in); //creates a new scanner object
 
-
-        System.out.print("Enter here the first number: ");
+System.out.println(start);
+        System.out.print("┇ Enter here the first number. ┇   ┠──> ");
             number1 = Input.nextDouble();
 
-            System.out.print("Enter here which operation you want to make (+, -, *, /, q for sqrt, p for pow, a for average, w for expression typing): ");
+            System.out.print("┇ Enter here which operation you want to make. ┇ [+, -, *, /, q for sqrt, p for pow, a for average, w for expression typing]   ┠──> ");
         operator = Input.next().charAt(0); //asks for an operator
        
 
             switch (operator) {
                 //performs addition between numbers
                 case '+':
-                System.out.print("Enter here the second number: ");
+                System.out.print("┇ Enter here the second number. ┇    ┠──> ");
                 number2 = Input.nextDouble();
                 result = number1 + number2;
                 System.out.println(number1 + " + " + number2 + " = " + result);
@@ -118,7 +158,7 @@ char operator;
 
                 //performs subtraction between numbers
                 case '-':
-                System.out.print("Enter here the second number: ");
+                System.out.print("┇ Enter here the second number. ┇    ┠──> ");
                 number2 = Input.nextDouble();
                 result = number1 - number2;
                 System.out.println(number1 + " - " + number2 + " = " + result);
@@ -126,7 +166,7 @@ char operator;
 
                 //performs multiplication between numbers
                 case '*':
-                System.out.print("Enter here the second number: ");
+                System.out.print("┇ Enter here the second number. ┇    ┠──> ");
                 number2 = Input.nextDouble();
                 result = number1 * number2;
                 System.out.println(number1 + " * " + number2 + " = " + result);
@@ -134,52 +174,66 @@ char operator;
 
                 //performs division between numbers
                 case '/':
-                System.out.print("Enter here the second number: ");
+                System.out.print("┇ Enter here the second number. ┇    ┠──> ");
                 number2 = Input.nextDouble();
                 result = number1 / number2;
                 resto = number1 % number2;
-                System.out.println(number1 + " / " + number2 + " = " + result + " with a rest of " + resto);
+                System.out.println("┇ " + number1 + " / " + number2 + " = " + result + " with a rest of " + resto + " ┇");
                 break;
 
                 //performs sqrt between numbers...?
                 case 'q':
                Double resultsqrt1 = Math.sqrt(number1);
-                System.out.print("First sqrt value: " + resultsqrt1);
+                System.out.print("┇ First sqrt value. ┇    ┠──> " + resultsqrt1);
 
                 break;
 
                 //performs pow
                 case 'p':
-                System.out.print("Enter here the second number: ");
+                System.out.print("┇ Enter here the second number. ┇    ┠──> ");
                 number2 = Input.nextDouble();
                 double resultpow1 = Math.pow(number1, number2);
-                System.out.println(number1 + " power of " +number2 + " = " +resultpow1);
+                System.out.println("┇ " + number1 + " power of " +number2 + " = " +resultpow1 + " ┇");
                 break;
 
                 //does an average between the two numbers
                 case 'a':
-                System.out.print("Enter here the second number: ");
+                System.out.print("┇ Enter here the second number. ┇    ┠──> ");
                 number2 = Input.nextDouble();
                 double number3;
                 
-                System.out.print("Enter here the third number to do an average: ");
+                System.out.print("┇ Enter here the third number to do an average. ┇    ┠──> ");
                 number3 = Input.nextDouble();
 
                 double resulta = (number1 + number2 + number3)/3;
-                System.out.println("The average between the two numbers is: " + resulta);
+                System.out.println("┇ The average between the two numbers is: " + resulta + " ┇");
                 break;
 
                 case 'w':
-                System.out.println("Enter here a mathematical expression as you would on any other mathematician program: ");
-                System.out.println(eval(Input.next()));
+                System.out.print("┇ Enter here a mathematical expression as you would on any other mathematician program. ┇    ┠──> ");
+                System.out.println("┇ Here is the result: ┇ " +eval(Input.next()));
 break;
             
             }
 
             System.out.println(" ");
-            System.out.println("Thank you for having used such simple, basic and poorly functional calculator.");
-            System.out.println("What you've been seeing is my testing projects for uni, which you can all check on my GitHub page at unimib.");
-            System.out.println(" You'll see me back in Visual Basic 6 because it is faaaar better than Java for me");
+
+            String end = """
+                    ╔════════════════════════════════════════════════════════════════════════════════════╗                        
+                    ║  We've reached the end.                                                        ┣X┨ ║
+                    ╚════════════════════════════════════════════════════════════════════════════════════╝    
+                   """;
+                           
+                           
+                    String end2 = """
+                     ║  This is the end of the calculator, one of the first things I've done in university║ 
+                     ║  I've yoinked some code here and there, but not that much, you can clearly see what║
+                     ║  is yoinked and what's not.                                                        ║
+                     ╚════════════════════════════════════════════════════════════════════════════════════╝
+                    """;
+                    
+                    System.out.print( ANSI_BOLD + end + ANSI_BOLD_END);
+                    System.out.print( ANSI_ORANGE1 +end2 + ANSI_RESET2);
 
             Input.close(); // after reading user input, you need to close it
 
---
+++ diciottoott.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+public class diciottoott {
+    
+
+public static void main (String[]args) {
+    // come controllare che il carattere sia una cifra
+    Scanner tastiera = new Scanner(System.in);
+    char carattere = tastiera.next().charAt(0);
+
+    System.out.println("You've entered this character --> " + carattere);
+
+    //controllare che il carattere sia una cifra
+    if ((carattere == '0') || (carattere == '1') || (carattere == '2'))
+    System.out.println("Looks like you've inserted a single number!");
+
+    if ((carattere == '0') && (carattere == '9'))
+        System.out.println("Damn");
+
+        //come vedere il numero dopo
+        carattere = (char) (carattere + 1);
+        System.out.println("The next character is: " + carattere);
+
+
+
+// provare se è vero e falso con int
+    int n = tastiera.nextInt();
+
+    if (n<0)
+    System.out.println(n<0); // n minore di zero
+    else if (n < 10)
+        System.out.println("n<10"); // n >= 0 e n<10
+        else if (n < 20)
+            System.out.println("n<20"); // n minore di 20
+            else
+                System.out.println("Erano tutte false rip");    
+                tastiera.close();
+}
+
+}
