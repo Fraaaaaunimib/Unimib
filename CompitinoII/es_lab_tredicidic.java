8c31cca7a99d115644027e65f0dca02bc2a70409
Francesco Cattaneo <f.cattaneo39@campus.unimib.it>
2022-12-15 18:04:05 +0000

    Tris now works

--- CompitinoII/es_lab_tredicidic.java
+++ CompitinoII/es_lab_tredicidic.java
@@ -331,18 +331,161 @@ System.out.println("There's already an element at this position, which is: "+ ar
                     */
 
     public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
        char [][] matrix = new char [3][3];
-       boolean finished = false;
+       boolean finished = false, winPC = false, winUser = false, repeat = true, validText = false, vediNonVincita = false, wrongMove = false;
+       int scelta = 0;
+
+       while (repeat == true){
+        
+        finished = false; winPC = false; winUser = false; repeat = true; validText = false; vediNonVincita = false; wrongMove = false;
+        scelta = sceltaMenu(scelta);
+
+        if (scelta == 1){
+            System.out.print("Player 1, enter your name --> ");
+            String name1 = sc.next();
+            System.out.println("Ok, " + name1 + ", ready to play? ");
+
        creaNuovaConfigurazione(matrix);
        while (finished == false){
         stampaMatrix(matrix);
+        if (wrongMove == false)
         mossaComputer(matrix);
-        mossaUser(matrix);
+        mossaUser(matrix,name1, wrongMove);
+        winPC = vediVincita(matrix);
+        winUser = vediVincitaUser(matrix);
+        vediNonVincita = vediNonVincita(matrix);
 
+        if (winPC == true){
+            finished = true;
+        }
+
+        if (winUser == true){
+            finished = true;
+        }
+        
+        if (vediNonVincita == true){
+            finished = true;
        }
 
+       if (winPC == true){
+        System.out.println("Sorry, but the computer won this time...");
     }
 
+    if (winUser == true){
+        System.out.println(name1 + ", you won! Yay!");
+    }
+
+    if (vediNonVincita = true){
+        System.out.println("Seems like no one has won. Perhaps you want to re-run again.");
+    }
+
+    while (validText == false){
+    System.out.println("Want to play again? \"S\" if you want, or \"N\" if you don't want. ");
+    String a = sc.next();
+    if (a.charAt(0) == 'S' || a.charAt(0) == 's'){
+        validText = true;
+        repeat = true;
+    } else if (a.charAt(0) == 'N' || a.charAt(0) == 'n'){
+        validText = true;
+        repeat = false;
+        System.exit(0);
+    } else {
+        System.out.println("You need to either insert a \"S\" if you want to repeat, or a \"N\" if you want to exit.");
+        validText= false;
+    }
+       }
+    }
+}
+
+    if (scelta == 2){
+
+        System.out.print("Player 1, enter your name --> ");
+        String name1 = sc.next();
+        System.out.println("Ok, " + name1 + ", ready to play? ");
+
+        System.out.print("Player 2, enter your name --> ");
+        String name2 = sc.next();
+        System.out.println("Perfect, " + name2 + ", ready to play?");
+        creaNuovaConfigurazione(matrix);
+        while (finished == false){
+         stampaMatrix(matrix);
+         if (wrongMove == false)
+         mossaUser(matrix, name1, wrongMove);
+         stampaMatrix(matrix);
+         if (wrongMove == false)
+         mossaUser2(matrix, name2, wrongMove);
+         winPC = vediVincita(matrix);
+         winUser = vediVincitaUser(matrix);
+         vediNonVincita = vediNonVincita(matrix);
+ 
+         if (winPC == true){
+             finished = true;
+         }
+ 
+         if (winUser == true){
+             finished = true;
+         }
+
+         if (vediNonVincita == true){
+            finished = true;
+         }
+        }
+
+        if (vediNonVincita == true){
+            System.out.println(name1 + ", and " + name2 + ", both of you have lost to this game.");
+            stampaMatrix(matrix);
+        }
+ 
+        if (winPC == true){
+         System.out.println("First player won!");
+     }
+ 
+     if (winUser == true){
+         System.out.println("Second player won!");
+     }
+ 
+     while (validText == false){
+     System.out.println("Want to play again? \"S\" if you want, or \"N\" if you don't want. ");
+     String a = sc.next();
+     if (a.charAt(0) == 'S' || a.charAt(0) == 's'){
+         validText = true;
+         repeat = true;
+     } else if (a.charAt(0) == 'N' || a.charAt(0) == 'n'){
+         validText = true;
+         repeat = false;
+         System.exit(0);
+     } else {
+         System.out.println("You need to either insert a \"S\" if you want to repeat, or a \"N\" if you want to exit.");
+         validText= false;
+     }
+        }
+     }
+    }
+
+
+    sc.close();
+}
+
+    public static int sceltaMenu(int scelta){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How do you want to play? Write \"1\" if you want to play against the computer, \"2\" if you want to play against someone else.");
+        scelta = sc.nextInt();
+        switch (scelta){
+            case 1:  
+            return 1;
+                
+            case 2:
+            return 2;
+            
+            default:
+                System.out.println("You have to insert either 1, if you want to play against the computer, or two if you want to play against another person.");
+                return 0;
+            
+        }
+
+    }
     public static void creaNuovaConfigurazione(char [][] matrix){
         for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
@@ -358,7 +501,11 @@ System.out.println("There's already an element at this position, which is: "+ ar
                     matrix[j][j+2] = 'X';
                 }
                 else if (matrix[j+1][j] == 'O'){
+                        try {
                     matrix[j+2][j] = 'X';
+                        } catch (IndexOutOfBoundsException e){
+                            System.out.print("Ignore this message.");
+                        }
                 }
             }
         }
@@ -390,17 +537,18 @@ System.out.println("There's already an element at this position, which is: "+ ar
         }
     }
 
-    public static void mossaUser(char[][]matrix){
+    public static void mossaUser(char[][]matrix, String name, boolean wrongMove){
         boolean valid = false, validInput = false;
         Scanner sc = new Scanner(System.in);
         int a = 0, b = 0;
         while (valid == false){
             while (validInput == false){
-        System.out.println("Which move you want to do? ");
+        System.out.print(name + ", which move you want to do? At which row: ");
         a = sc.nextInt();
+        System.out.print("And in which column? ");
         b = sc.nextInt();
 
-        if (a > 0 && a < 3 && b > 0 && b < 3){
+        if (a > 0 && a < 4 && b > 0 && b < 4){
             validInput = true;
         } else {
             System.out.println("Nope, not valid.");
@@ -409,27 +557,117 @@ System.out.println("There's already an element at this position, which is: "+ ar
 
         a--;
         b--;
-
+            try {
         if (matrix[a][b] == 'X'){
             System.out.println("Nope, this is not valid.");
+            wrongMove = true;
+            break;
         } else if (matrix[a][b] == ' '){
         matrix[a][b] = 'O';
+        wrongMove = false;
         valid = true;
+        } else if (matrix[a][b] == 'O'){
+            System.out.println("You can't place your move on top of an already occupied place.");
+            wrongMove = true;
+            break;
         }
+    } catch (IndexOutOfBoundsException e){
+        System.out.print("");
+    }
+    }
+    }
+
+    public static void mossaUser2(char[][]matrix, String name, boolean wrongMove){
+        boolean valid = false, validInput = false;
+        Scanner sc = new Scanner(System.in);
+        int a = 0, b = 0;
+        while (valid == false){
+            while (validInput == false){
+        System.out.print(name + ", which move you want to do? At which row: ");
+        a = sc.nextInt();
+        System.out.print("And in which column? ");
+        b = sc.nextInt();
+
+        if (a > 0 && a < 4 && b > 0 && b < 4){
+            validInput = true;
+        } else {
+            System.out.println("Nope, not valid.");
+        }
+            }
+
+        a--;
+        b--;
+            try {
+        if (matrix[a][b] == 'O'){
+            System.out.println("Nope, this is not valid.");
+            wrongMove = true;
+            break;
+        } else if (matrix[a][b] == ' '){
+        matrix[a][b] = 'X';
+        valid = true;
+        wrongMove = false;
+        }  else if (matrix[a][b] == 'X'){
+            System.out.println("You can't place your move on top of an already occupied place.");
+            wrongMove = true;
+            break;
+        }
+    } catch (IndexOutOfBoundsException e){
+        System.out.print("");
+    }
     }
     }
 
     public static void stampaMatrix(char[][]matrix){
-        System.out.println(" _    _    _ ");
+        System.out.println("  1     2     3 ");
+        System.out.println("  _     _     _ ");
         for (int i = 0; i < 3; i++){
+            System.out.print ((i+1 + "  "));
             for (int j = 0; j < 3; j++){
                 System.out.print(" " + matrix[i][j] + " | ");
             }
             System.out.println("");
-            System.out.println(" _    _    _ ");
+            System.out.println("  _     _     _ ");
         }
     }
     
+    public static boolean vediVincita(char[][]matrix){
+        boolean vincitaComputer = false;
+        if (matrix[0][0] == 'X' && matrix[1][0] == 'X' && matrix[2][0] == 'X' || matrix[0][0] == 'X' && matrix[0][1] == 'X' && matrix[0][2] == 'X' || matrix[0][0] == 'X' && matrix [1][1] == 'X' && matrix[2][2] == 'X' || matrix[0][2] == 'X' && matrix[1][1] == 'X' && matrix[2][0] == 'X' || matrix[0][1] == 'X' && matrix[1][1] == 'X' && matrix[2][1] == 'X' || matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X' || matrix[1][0] == 'X' && matrix[1][1] == 'X' && matrix[1][2] == 'X' || matrix[2][0] == 'X' && matrix[2][1] == 'X' && matrix[2][2] == 'X'){
+            vincitaComputer = true;
+        } else {
+            vincitaComputer = false;
+        }
+
+        return vincitaComputer; 
+    }
+
+    public static boolean vediVincitaUser(char[][]matrix){
+        boolean vincitaUser = false;
+        if (matrix[0][0] == 'O' && matrix[1][0] == 'O' && matrix[2][0] == 'O' || matrix[0][0] == 'O' && matrix[0][1] == 'O' && matrix[0][2] == 'O' || matrix[0][0] == 'O' && matrix [1][1] == 'O' && matrix[2][2] == 'O' || matrix[0][2] == 'O' && matrix[1][1] == 'O' && matrix[2][0] == 'O' || matrix[0][1] == 'O' && matrix[1][1] == 'O' && matrix[2][1] == 'O' || matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O' || matrix[1][0] == 'O' && matrix[1][1] == 'O' && matrix[1][2] == 'O' || matrix[2][0] == 'O' && matrix[2][1] == 'O' && matrix[2][2] == 'O'){
+            vincitaUser = true;
+        } else {
+            vincitaUser = false;
+        }
+
+        return vincitaUser; 
+    }
+
+    public static boolean vediNonVincita(char[][] matrix){
+        boolean isItNonFinished = false;
+
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                if (matrix[i][j] == ' '){
+                    isItNonFinished = false;
+                    break;
+                } else {
+                    isItNonFinished = true;
+                }
+            }
+        }
+
+        return isItNonFinished;
+    }
                  }
              
             
